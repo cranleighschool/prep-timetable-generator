6bd56756968a156ab4d44e6a99ba1368f5368839
Fred Bradley <code@fredbradley.co.uk>
2022-01-04 13:51:36 +0000

    fix typo

--- resources/views/timetable.blade.php
+++ resources/views/timetable.blade.php
@@ -68,7 +68,7 @@
             </button>
             <a class="btn btn-lg btn-success" id="download" download="{{ $request->username }}.png"><svg xmlns="http://www.w3.org/2000/svg" class="" style="width:24px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
-                </svg>Download as Image</a>q
+                </svg>Download as Image</a>
         </div>
     </div>
 @endsection
