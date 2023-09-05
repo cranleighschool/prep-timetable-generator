55988eb7fa3eba9d81031aaffa7e9bae33d39ed9
Fred Bradley <code@fredbradley.co.uk>
2023-09-05 22:17:38 +0100

    comment out a broken function

--- resources/views/timetable.blade.php
+++ resources/views/timetable.blade.php
@@ -61,11 +61,11 @@
     <div class="row p-3">
         <div style="display: none;" id="hidden-photo"></div>
         <div class="col">
-            <button type="button" class="btn btn-lg btn-success" id="copy-to-clipboard"
-                    onclick="CopyToClipboard('timetable-card')"><svg xmlns="http://www.w3.org/2000/svg" style="width:24px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-                </svg>Copy Table Contents to Clipboard
-            </button>
+{{--            <button type="button" class="btn btn-lg btn-success" id="copy-to-clipboard"--}}
+{{--                    onclick="CopyToClipboard('timetable-card')"><svg xmlns="http://www.w3.org/2000/svg" style="width:24px;" fill="none" viewBox="0 0 24 24" stroke="currentColor">--}}
+{{--                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />--}}
+{{--                </svg>Copy Table Contents to Clipboard--}}
+{{--            </button>--}}
             <a class="btn btn-lg btn-success" id="download" download="{{ $request->username }}.png"><svg xmlns="http://www.w3.org/2000/svg" class="" style="width:24px" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
                 </svg>Download as Image</a>
