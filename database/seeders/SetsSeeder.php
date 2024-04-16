a7e3bd9a5946861dad0e2aa14c719e2fd9daea7a
Fred Bradley <code@fredbradley.co.uk>
2024-04-16 12:38:56 +0100

    fix: mistake

--- database/seeders/SetsSeeder.php
+++ database/seeders/SetsSeeder.php
@@ -160,7 +160,7 @@ class SetsSeeder extends Seeder
         ]);
 
         $this->setsBySubjectToday('Physics', 10, GenerateTimetable::WEDNESDAY, [
-            'D91',
+            'D91', 'T4', 'T5',
         ]);
         $this->setsBySubjectToday('Biology', 10, GenerateTimetable::WEDNESDAY, [
             'T1', 'T2', 'T3', 'D92', 'D6',
